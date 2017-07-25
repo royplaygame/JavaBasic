44c834e6bd6ca6f35eaaa69e5bc7fc5549563e8d
root <root@test.com>
2017-07-25 20:38:07 +0800

    equals

---
+++ src/com/hy/ly/basic/EqualsTest.java
@@ -0,0 +1,41 @@
+package com.hy.ly.basic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.hy.ly.po.Student;
+
+public class EqualsTest {
+
+	public static void main(String[] args) {
+		Student stu1 = new Student(1001, "zhangsan", "男", "18399991111");
+		Student stu2 = new Student(1001, "zhangsan", "男", "18399991111");
+		Student stu4 = new Student(1005, "zhangsan", "女", "18399991111");
+		Student stu3 = stu1;
+
+		System.out.println(stu1 == stu2);
+		System.out.println(stu1.equals(stu2));
+
+		System.out.println(stu1 == stu3);
+		System.out.println(stu1.equals(stu3));
+
+		int hashCode = stu1.hashCode();
+		System.out.println(hashCode);
+		System.out.println(stu2.hashCode());
+		System.out.println(stu3.hashCode());
+		System.out.println(stu4.hashCode());
+		
+		
+		String []hobbys=new String[5];
+		hobbys[0]="pingpang";
+		hobbys[1]="yumaoqiu";
+		hobbys[2]="lanqiu";
+		hobbys[3]="zuqiu";
+		hobbys[4]="wangqiu";
+		
+		List<String> list=Arrays.asList(hobbys);
+		for(String s:list){
+			System.out.println(s);
+		}
+	}
+}
---
+++ src/com/hy/ly/po/Student.java
@@ -0,0 +1,91 @@
+package com.hy.ly.po;
+
+import java.util.Objects;
+
+public class Student {
+
+	private Integer id;
+	private String name;
+	private String sex;
+	private String phone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// 如果为同一对象的不同引用,则相同
+		if (this == obj) {
+			return true;
+		}
+
+		// 如果传入的对象为空,则返回false
+		if (obj == null) {
+			return false;
+		}
+
+		// 如果两者属于不同的类型,不能相等
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		// 类型相同, 比较内容是否相同
+		Student stu = (Student) obj;
+
+		return Objects.equals(name, stu.name) && id == stu.id && Objects.equals(sex, stu.sex)
+				&& Objects.equals(phone, stu.phone);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		return result;
+	}
+
+	public Student() {
+		super();
+	}
+
+	public Student(Integer id, String name, String sex, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sex = sex;
+		this.phone = phone;
+	}
+
+}
