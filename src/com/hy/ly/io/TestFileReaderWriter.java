927fc21cac7100f2de11917adfecb9832a8558fb
root <root@test.com>
2017-07-25 21:39:53 +0800

    FileWriter and FileReader

--- src/com/hy/ly/io/TestFileReaderWriter.java
+++ src/com/hy/ly/io/TestFileReaderWriter.java
@@ -1,18 +1,131 @@
 package com.hy.ly.io;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.junit.Test;
 
 public class TestFileReaderWriter {
 
 	@Test
-	public void testFileReader() throws FileNotFoundException {
-		File file = new File("D:\\temp\\tt.doc");
+	public void testFileReader(){
+		File file = new File("D:\\tmp\\customer1.sql");
+		FileReader fr=null;
 
-		FileReader fr = new FileReader(file);
+		try {
+			fr = new FileReader(file);
+			
+			char []c=new char[1024];
+			int len;
+			while((len=fr.read(c))!=-1){
+				String str=new String(c,0,len);
+				System.out.println(str);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			if(fr!=null){
+				try {
+					fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	@Test
+	public void testFileWriter(){
+		
+		File file=new File("D:\\tmp\\good.txt");
+		FileWriter fw=null;
+		try{
+			fw=new FileWriter(file);
+			fw.write(new String("this is good for my card").toCharArray());
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			if(fw!=null){
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
+	@Test
+	public void testFileReaderWriter(){
+		File source=new File("D:\\tmp\\work.txt");
+		File dest=new File("D:\\tmp\\task.txt");
+		
+		FileReader fr=null;
+		FileWriter fw=	null;
+		try{
+			fr=new FileReader(source);
+			fw=new FileWriter(dest);
+			
+			char []c=new char[100];
+			int len;
+			while((len=fr.read(c))!=-1){
+				fw.write(c, 0, len);
+			}
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			if(fr!=null){
+				try {
+					fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(fw!=null){
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public void copyFile(String source,String dest){
+		FileReader fr=null;
+		FileWriter fw=	null;
+		try{
+			fr=new FileReader(new File(source));
+			fw=new FileWriter(new File(dest));
+			
+			char []c=new char[100];
+			int len;
+			while((len=fr.read(c))!=-1){
+				fw.write(c, 0, len);
+			}
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			if(fr!=null){
+				try {
+					fr.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(fw!=null){
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
