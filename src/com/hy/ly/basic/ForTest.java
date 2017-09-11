d297d920e9a2260803524bff41a2d0343835f80b
root <root@test.com>
2017-09-11 21:05:34 +0800

    update

--- src/com/hy/ly/basic/ForTest.java
+++ src/com/hy/ly/basic/ForTest.java
@@ -1,5 +1,9 @@
 package com.hy.ly.basic;
 
+import java.util.Scanner;
+
+import org.junit.Test;
+
 public class ForTest {
 
 	public static void main(String[] args) {
@@ -12,4 +16,25 @@ public class ForTest {
 			obj.equals("");
 		}
 	}
+	
+	@Test
+	public void test(){
+		//输入一串已知字符串，再输入一串子字符串，输出该子字符串在已知字符串中出现的次数
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("请输入一串字符串");
+		String str = scanner.nextLine();
+		System.out.println("请再输入一串子字符串");
+		String str1 = scanner.nextLine();
+
+		int count = 0;
+		int start = 0;
+		while (str.indexOf(str1, start) > 0 && start < str.length()) {
+		count++;
+		start = str.indexOf(str1, start) + str1.length();
+		}
+
+		System.out.println(count);
+		scanner.close();
+	}
+	
 }
